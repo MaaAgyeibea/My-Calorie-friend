576c413e7d1820b65d1d73397e998b9f12dd03c0
Hilda <hamohenaku@gmail.com>
2018-02-01 18:57:31 +0200

    worked on the  SetActivity layout

--- app/src/main/java/com/caloriebuddie/app/ui/activities/SetupActivity.java
+++ app/src/main/java/com/caloriebuddie/app/ui/activities/SetupActivity.java
@@ -1,9 +1,19 @@
 package com.caloriebuddie.app.ui.activities;
 
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
 /**
  * Created by tette on 30/01/2018.
  */
 
 public class SetupActivity extends BaseActivity {
+TextView AgeOfUser, HeightofUser, CurrentWeight, unitforage, unitforheight, unitforweight, Gender, diaseselect, Activeness;
+EditText Weight, Height, Age;
+CheckBox Male, Female, dbtype1, dbtype2;
+Spinner Spinner;
+
 
 }
